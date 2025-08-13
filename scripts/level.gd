d3eea7fedbeb2393998a479ac5bf0c336eb9a376
Michal Kozubik <michal.kozubik@sap.com>
2025-08-13 08:27:22 +0200

    Fix touch controls

--- scripts/level.gd
+++ scripts/level.gd
@@ -26,8 +26,8 @@ func _process(delta: float) -> void:
 	check_system_inputs()
 
 func check_system_inputs() -> void:
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
+	#if Input.is_action_just_pressed("quit"):
+		#get_tree().quit()
 	if Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
 
--- scripts/ui_layer.gd
+++ scripts/ui_layer.gd
@@ -2,33 +2,27 @@ class_name UILayer
 extends CanvasLayer
 
 func _ready() -> void:
-	var is_touchscreen = DisplayServer.is_touchscreen_available()
+	$Controls.visible = DisplayServer.is_touchscreen_available()
 	
-	$Controls/Left.visible = is_touchscreen
-	$Controls/Right.visible = is_touchscreen
-	$Controls/Jump.visible = is_touchscreen
-	
-	connect_virtual_button($Controls/Left, "move_left")
-	connect_virtual_button($Controls/Right, "move_right")
+	connect_virtual_button($Controls/MoveLeft, "move_left")
+	connect_virtual_button($Controls/MoveRight, "move_right")
 	connect_virtual_button($Controls/Jump, "jump")
 
 func show_win_screen(flag: bool) -> void:
 	$WinScreen.visible = flag
-	$Controls.visible = !flag
+	$Controls.visible = !flag && DisplayServer.is_touchscreen_available()
 		
 
-func connect_virtual_button(btn: Button, action: StringName):
-	btn.button_down.connect(create_button_event_handler(action, true))
-	btn.button_up.connect(create_button_event_handler(action, false))
+func connect_virtual_button(btn: TouchScreenButton, action: StringName):
+	btn.pressed.connect(func():
+		fire_input_event(action, true)
+	)
+	btn.released.connect(func():
+		fire_input_event(action, false)
+	)
 
 func fire_input_event(action: StringName, pressed: bool):
 	var event = InputEventAction.new()
 	event.action = action
 	event.pressed = pressed
 	Input.parse_input_event(event)
-
-func create_button_event_handler(action: StringName, pressed: bool):
-	var handler = func():
-		fire_input_event(action, pressed)
-		
-	return handler
